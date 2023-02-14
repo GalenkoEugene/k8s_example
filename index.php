f1cdc8625049ecc66ad0fb471d65f997e6528868
Yevhenii Halenko <Re4port@ukr.net>
2023-02-14 10:58:52 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello from Kubernetes</h1><br>";
+echo "Server IP address is: $ip_server", "<br><p>";
+echo "Made by <font color=blue>Yevhenii Halenko";
+
+?>
